c013d26bae39ad0b8b0bfc4b7405100984d80ccc
gyozonagy <gyozo.nagyv@hello.hu>
2014-08-21 00:04:33 +0200

    adding some remarks

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is computationally expensive. We compute the inverted matrix ones, and 
+## caches the result into variable "s". Upon subsequent uses we recall that object
 
-## Write a short comment describing this function
+## a list of four auxiliary function to set and get the matrix, and the inverted matrix. (setsolv,getsolv)
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -13,26 +13,32 @@ makeCacheMatrix <- function(x = matrix()) {
         get <- function() x
         setsolv <- function(solve) s <<- solve
         getsolv <- function() s
+
+        ## returns the list of four auxiliary function
         list(set = set, get = get,
              setsolv = setsolv,
              getsolv = getsolv)
-
-
 }
 
 
-## Write a short comment describing this function
+## compute or fetch the inverted matrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-		        s <- x$getsolv()
+        ## try to fetch the inverse into s
+        s <- x$getsolv()
+
+        ## if s is not empty, return it!
         if(!is.null(s)) {
                 message("getting cached data")
                 return(s)
         }
+        ## otherwise compute it by function "solve"
         data <- x$get()
         s <- solve(data, ...)
         x$setsolv(s)
+
+        ## and return the computed object:
         s
 
 }
